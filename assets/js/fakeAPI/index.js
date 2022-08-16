1bdf8711a8ebb81aa88a04c66acf3b23dc6522df
Andressa Da Costa <andressadacostan@gmail.com>
2022-08-16 01:33:04 -0300

    API

---
+++ assets/js/fakeAPI/index.js
@@ -0,0 +1,207 @@
+export const lista = [
+	{
+		titulo: "Star Wars",
+		produtos: [
+			{
+				infos: {
+					id: 1,
+					nome: "Capacete Stormtrooper",
+					img: "/assets/img/capacete.svg",
+					preco: 60.01,
+					descricao:
+						"Os Stormtroopers são a evolução dos antigos Clone Troopers das Guerras Clônicas e são superiores a eles em tudo. Sua armadura era mais resistente e bem equipada, suas armas eram superiores até sua devoção é mais sólida do que a dos soldados clones.",
+					alt: "Capacete branco"
+				}
+			},
+			{
+				infos: {
+					id: 2,
+					nome: "Lego Dart Vader",
+					img: "/assets/img/capacete-dart-vader.svg",
+					preco: 60.02,
+					descricao:
+						"N'Kata Del Gormo foi um Mestre Jedi Hysalriano que viveu durante os tempos da República Galáctica. Ele encontrou e treinou Yoda e seu amigo Humano ao notar a capacidade de ambos influenciada pela Força.",
+					alt: "Extra terrestre verde."
+				}
+			},
+			{
+				infos: {
+					id: 3,
+					nome: "Mestre Yoda",
+					img: "/assets/img/yoda.svg",
+					preco: 60.03,
+					descricao:
+						"N'Kata Del Gormo foi um Mestre Jedi Hysalriano que viveu durante os tempos da República Galáctica. Ele encontrou e treinou Yoda e seu amigo Humano ao notar a capacidade de ambos influenciada pela Força.",
+					alt: "Extra terrestre verde"
+				}
+			},
+			{
+				infos: {
+					id: 4,
+					nome: "Stormtroopers",
+					img: "/assets/img/stormtroopers.svg",
+					preco: 60.04,
+					descricao:
+						"Os Stormtroopers são a tropa de base do Império Galáctico no universo Star Wars.",
+					alt: "Soldado Starwars"
+				}
+			},
+			{
+				infos: {
+					id: 5,
+					nome: "Baby Yoda",
+					img: "/assets/img/baby-yoda.svg",
+					preco: 60.05,
+					descricao:
+						"Grogu, inicialmente chamado de A criança, popularmente conhecido como Baby Yoda, é um personagem fictício do universo Star Wars, que apareceu pela primeira vez na série The Mandalorian, do streaming Disney Plus;",
+					alt: "Grogu Baby Yoda"
+				}
+			},
+			{
+				infos: {
+					id: 6,
+					nome: "Dart Vader",
+					img: "/assets/img/dart-vader.svg",
+					preco: 60.06,
+					descricao:
+						"Star Wars (Brasil: Guerra nas Estrelas / Portugal: Guerra das Estrelas) é uma franquia do tipo space opera estadunidense criada pelo cineasta George Lucas, que conta com uma série de nove filmes de fantasia científica e dois spin-offs.",
+					alt: "Homem com máscara e capacete preto"
+				}
+			}
+		]
+	},
+	{
+		titulo: "Consoles",
+		produtos: [
+			{
+				infos: {
+					id: 1,
+					nome: "Controle XBox",
+					img: "/assets/img/controleXbox.svg",
+					preco: 60.07,
+					descricao:
+						"Imagem 3 de 3 de Controle joystick sem fio Microsoft Xbox Wireless Controller Series X|S robot white Novo  |  12800 vendidos Controle joystick sem fio Microsoft Xbox Wireless Controller Series X|S robot white",
+					alt: "Controle de videogame"
+				}
+			},
+			{
+				infos: {
+					id: 2,
+					nome: "Controle e console Play5",
+					img: "/assets/img/consoleEcontrole.svg",
+					preco: 60.08,
+					descricao:
+						"Jogar não Tem Limites. Desfrute do carregamento extremamente rápido com o SSD de altíssima velocidade, uma imersão mais profunda com suporte a feedback tátil, gatilhos adaptáveis e áudio 3D, além de uma geração inédita de jogos incríveis para Console Playstation 5 - PS5",
+					alt: "Console e controle do Playstation 5"
+				}
+			},
+			{
+				infos: {
+					id: 3,
+					nome: "Console Nintendo",
+					img: "/assets/img/ConsoleNintendo.svg",
+					preco: 60.09,
+					descricao: "Console Nintendo antigo - Retrô",
+					alt: "Console Nintendo antigo"
+				}
+			},
+			{
+				infos: {
+					id: 4,
+					nome: "Console Nintendo Switch",
+					img: "/assets/img/NintendoSwitch.svg",
+					preco: "60.10",
+					descricao: "",
+					alt: "Console Nintendo Switch"
+				}
+			},
+			{
+				infos: {
+					id: 5,
+					nome: "Console Xbox 360",
+					img: "/assets/img/xBox.svg",
+					preco: 60.11,
+					descricao: "",
+					alt: "Console Xbox 360"
+				}
+			},
+			{
+				infos: {
+					id: 6,
+					nome: "Game Boy Color",
+					img: "/assets/img/gameBoy.svg",
+					preco: 60.12,
+					descricao: "",
+					alt: "Game boy game"
+				}
+			}
+		]
+	},
+	{
+		titulo: "Diversos",
+		produtos: [
+			{
+				infos: {
+					id: 1,
+					nome: "Camisa Atari",
+					img: "/assets/img/camisaAtari.svg",
+					preco: 60.13,
+					descricao:
+						"One Piece é uma série de mangá escrita e ilustrada por Eiichiro Oda. Os capítulos têm sido serializados na revista Weekly Shōnen Jump desde 22 de julho de 1997, com os capítulos compilados e publicados em 100 volumes tankōbon pela editora Shueisha até setembro de 2021.",
+					alt: "Adolescente com chapéu de palha"
+				}
+			},
+			{
+				infos: {
+					id: 2,
+					nome: "Camisa SNES",
+					img: "/assets/img/camisaSNES.svg",
+					preco: 60.14,
+					descricao: "",
+					alt: "Camisa SNES"
+				}
+			},
+			{
+				infos: {
+					id: 3,
+					nome: "Boneco Sonic",
+					img: "/assets/img/sonicToy.svg",
+					preco: 60.15,
+					descricao: "",
+					alt: "Boneco Sonic"
+				}
+			},
+			{
+				infos: {
+					id: 4,
+					nome: "Rádio Antigo Rosa",
+					img: "/assets/img/radioPink.svg",
+					preco: 60.16,
+					descricao: "",
+					alt: "Rádio Antigo Rosa"
+				}
+			},
+			{
+				infos: {
+					id: 5,
+					nome: "Óculos VR",
+					img: "/assets/img/vrGlasses.svg",
+					preco: 60.17,
+					descricao: "",
+					alt: "Homem usando óculos de realidade virtual"
+				}
+			},
+			{
+				infos: {
+					id: 6,
+					nome: "Pikachu Fantasia",
+					img: "/assets/img/pikachu.svg",
+					preco: 60.18,
+					descricao:
+						"Pikachu é uma espécie fictícia pertencente à franquia de mídia Pokémon da Nintendo. Ele apareceu pela primeira vez no Japão em 1996, nos jogos eletrônicos Pokémon Red and Blue, e foi criado por Satoshi Tajiri.",
+					alt: "Pokémon Pikachu"
+				}
+			}
+		]
+	}
+]
